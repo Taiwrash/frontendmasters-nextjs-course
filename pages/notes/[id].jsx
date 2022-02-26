bf22bd636eacf34ce6f892e0358fce27acd7d46e
mudasiru rasheed <rasheedrtm1@gmail.com>
2022-02-26 22:08:29 +0100

    Completing nextjs course on frontend master

---
+++ pages/notes/[id].jsx
@@ -0,0 +1,33 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const Note = ({note}) => {
+  return (
+    <div sx={{variant: 'containers.page'}}>
+      <h1>Note: {note.title} </h1>
+    </div>
+  )
+  }
+
+export default Note;
+
+export async function getServerSideProps({params, req, res}) {
+  const response = await fetch(`${process.env.APP_URL}/api/note/${params.id}`)
+
+  // so much power!
+  if (!response.ok) {
+    res.writeHead(302, { Location: '/notes' })
+    res.end()
+    return {props: {}}
+  }
+
+  const {data} = await response.json()
+  
+  if (data) {
+    return {
+      props: {note: data}
+    }
+  }
+}
